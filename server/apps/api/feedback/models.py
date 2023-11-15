818a7f551179a929eff27b593d8011368012ab32
AndreEva <app.re.app@icloud.com>
2023-11-15 14:40:50 +0300

    trying to understand models and qform

--- server/apps/api/feedback/models.py
+++ server/apps/api/feedback/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Feedback(models.Model):
+    """
+    Модель заявки на курс
+    """
+
+    name = models.CharField(_("Имя"), max_length=70, null=True, blank=False)
+    phone = models.CharField(_("Телефон"), max_length=11, null=True, blank=False)
+    email = models.EmailField(
+        _("Email"), max_length=254, null=True, blank=False
+    )
+ 
+
+    def __str__(self):
+        return self.name
--- src/components/AppFeedback.vue
+++ src/components/AppFeedback.vue
@@ -1,21 +1,14 @@
 <template>
 
-    <form>
-        <div class="row">
-            <div class="col">
-                <input>
-            </div>
-        </div>
 
-    </form>
-    <q-form>
+    <q-form @submit.prevent="onSubmit" ref="feedbackForm" >
         <q-input outlined v-model="name" label="Имя" :rules="[(val) => !!val || 'Обязательное поле']"></q-input>
         <q-input outlined v-model="phone" label="Телефон" :rules="[(val) => !!val || 'Обязательное поле']"></q-input>
         <q-input 
         outlined 
         v-model="email" 
         label="E-mail" 
-        :rules="[(val) => !!val || 'Обязательное поле']"
+        :rules="[(val) => !!val || 'Обязательное поле', isValidEmail()]"
         ></q-input>
         <q-btn
           flat 
@@ -29,9 +22,22 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+//import { api } from 'boot/axios';
+import { QForm } from 'quasar';
 const name = ref(''),
 email = ref(''),
 phone = ref('');
-
+//const feedbackForm = ref<QForm>();//?
+const isValidEmail = () => {
+    const emailPattern =
+      /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+    return (val: string) => emailPattern.test(val) || 'Неверный формат Email';
+  };
+  const onSubmit = () => {
+// магия
+// void feedbackForm.value?.validate().then(async (success: boolean) => {
+//       if (success) {
+//       }}
+}
 
 </script>
