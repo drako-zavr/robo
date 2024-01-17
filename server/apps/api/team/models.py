f1403462c6bebad12795db155417d547a1332f50
AndreEva <app.re.app@icloud.com>
2024-01-17 11:07:59 +0300

    fix models

--- server/apps/api/team/models.py
+++ server/apps/api/team/models.py
@@ -6,13 +6,13 @@ class Teacher(models.Model):
     Модель преподавателя
     """
 
-    name = models.CharField(verbose_name=_("Имя"), max_length=255)
-    surname = models.CharField(verbose_name=_("Фамилия"), max_length=255)
-    photo = models.ImageField(verbose_name=_("Фотография"), blank=True)
-    position = models.CharField(verbose_name=_("Должность"), max_length=255)
-    social1 = models.CharField(verbose_name=_("Ссылка на соцсеть 1"), max_length=255)
-    social2 = models.CharField(verbose_name=_("Ссылка на соцсеть 2"), max_length=255)
-    info = models.CharField(verbose_name=_("Информация"), max_length=255)
+    name = models.CharField(verbose_name=_("Имя"), max_length=255, null=True)
+    surname = models.CharField(verbose_name=_("Фамилия"), max_length=255, null=True)
+    photo = models.ImageField(verbose_name=_("Фотография"), blank=True, null=True)
+    position = models.CharField(verbose_name=_("Должность"), max_length=255, null=True)
+    social1 = models.CharField(verbose_name=_("Ссылка на соцсеть 1"), max_length=255, null=True)
+    social2 = models.CharField(verbose_name=_("Ссылка на соцсеть 2"), max_length=255, null=True)
+    info = models.CharField(verbose_name=_("Информация"), max_length=255, null=True)
 
     def __str__(self):
         return self.name
