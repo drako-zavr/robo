e9ea80460eaddc1a50a603ddc61c80d6691d6914
AndreEva <app.re.app@icloud.com>
2024-01-22 13:17:53 +0300

    reduce css

--- src/components/AppFeedback.vue
+++ src/components/AppFeedback.vue
@@ -1,11 +1,11 @@
 <template>
   <SectionBlock  
-  bgClass="feedback-section"
+  bgClass="feedback-section q-pb-xl"
   >
-    <div id="feedback" class="feedback q-container ">
+    <div id="feedback" class="feedback q-container q-pb-xl">
       <div class="row q-mx-auto justify-around">
         <div class="feedback__desc col-6-md col-6-lg col-12-sm text-white q-px-sm q-mb-xl justify-center">
-          <p class="feedback__title q-mb-md">Запишитесь на курс <br>со скидкой 10%</p>
+          <p class="feedback__title q-mb-md text-h4">Запишитесь на курс <br>со скидкой 10%</p>
           <p class="feedback__text text-weight-medium"> Акция действительна до 10 марта 2022 года</p>
         </div>
         <FeedbackForm />
--- src/layouts/AppFooter.vue
+++ src/layouts/AppFooter.vue
@@ -1,8 +1,8 @@
 <template>
     <q-footer class="bg-secondary footer ">
         <div class="row justify-between">
-            <p class="col-6-md col-6-lg col-12-sm footer__title text-weight-bold ">ROBO.SCHOOL</p>
-            <p class="col-6-md col-6-lg col-12-sm footer__phone text-right ">+7 800 000 11 22</p>
+            <p class="col-6-md col-6-lg col-12-sm footer__title text-weight-bold text-h4">ROBO.SCHOOL</p>
+            <p class="col-6-md col-6-lg col-12-sm footer__phone text-right text-h4">+7 800 000 11 22</p>
         </div>
         <p class="footer__text">© ROBO.SCHOOL</p>
     </q-footer>
